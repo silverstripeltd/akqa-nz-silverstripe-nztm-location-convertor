738b88d308370b1e6a04ea4c6840a017f95d8422
Tim Higgs <tim.higgs@heyday.co.nz>
2017-03-21 17:52:19 +1300

    Add CoOrdinates class

---
+++ code/CoOrdinates.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * CoOrdinates
+ */
+class CoOrdinates
+{
+    const DECIMAL_PLACES = 4;
+
+    public $latitude;
+    public $longitude;
+
+
+    /**
+     * @return mixed
+     */
+    public function getFormattedLatitude()
+    {
+        return floatval($this->latitude * TMProjection::RAD2DEG);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFormattedLongitude()
+    {
+        return floatval($this->longitude * TMProjection::RAD2DEG);
+    }
+
+
+    /**
+     * @param $number
+     * @return float
+     */
+    public static function truncateNumber($number)
+    {
+        return floatval(bcdiv($number, 1, self::DECIMAL_PLACES));
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        $latitude = $this->getFormattedLatitude();
+        $longitude = $this->getFormattedLongitude();
+
+        return "{$latitude}, {$longitude}";
+    }
+
+}
+
